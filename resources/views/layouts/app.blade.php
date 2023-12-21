96911565ee73490f97780fd0f777daaae7bf6504
haruka-ochiai <pw.g1.po1122@gmail.com>
2023-12-21 18:23:54 +0900

    add table

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,13 @@
         <script src="https://cdn.tailwindcss.com"></script>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="/css/color.css">
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <body class="font-sans antialiased background-color">
+        <div class="min-h-screen ">
+            <!-- 共通ヘッダー -->
+            @include('layouts.announce-header')
+            @include('layouts.global-header')
 
             <!-- Page Heading -->
             @if (isset($header))
@@ -33,5 +36,6 @@
                 {{ $slot }}
             </main>
         </div>
+        @include('layouts.footer')
     </body>
 </html>
